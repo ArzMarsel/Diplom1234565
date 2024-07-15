6db73a34068a24fec162dc746398d0f7a91f42a3
ArzMarsel <marselarzybekov3@gmail.com>
2024-07-15 17:40:15 +0600

    m1

--- Diplom/main/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Dish, DishImage, Connect, Payment
-
-admin.site.register(Dish)
-admin.site.register(DishImage)
-admin.site.register(Connect)
-admin.site.register(Payment)
